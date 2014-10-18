6a1e5372da9777141370ddebcce116d5597a44ab
David Franke <mirroar@gmail.com>
2014-10-18 13:27:47 +0200

    some more code cleaning

--- utilities.php
+++ utilities.php
@@ -2,16 +2,11 @@
 
 define('DOWNLOAD_LIMIT', 50);
 
+/**
+ * Print debug output
+ */
 function debug($text) {
   echo $text . '<br>';
-  ob_flush();
-}
-
-function cURLcheckBasicFunctions() {
-  if(!function_exists("curl_init") && !function_exists("curl_setopt") && !function_exists("curl_exec") && !function_exists("curl_close")) {
-    return false;
-  }
-  return true;
 }
 
 /**
@@ -19,50 +14,35 @@ function cURLcheckBasicFunctions() {
  * Can be changed to int or bool return types.
  */
 function cURLdownload($url, $file, $redirects = 30) {
-  if(!cURLcheckBasicFunctions())
-    return "UNAVAILABLE: cURL Basic Functions";
   $ch = curl_init();
-  if($ch) {
+  if ($ch) {
     $fp = fopen($file, "w");
-    if($fp) {
-      if(!curl_setopt($ch, CURLOPT_URL, $url)) {
+    if ($fp) {
+      if (!curl_setopt($ch, CURLOPT_URL, $url)) {
         fclose($fp); // to match fopen()
         curl_close($ch); // to match curl_init()
-        return "FAIL: curl_setopt(CURLOPT_URL)";
+        return;
       }
       if ((!ini_get('open_basedir') && !ini_get('safe_mode')) || $redirects < 1) {
         curl_setopt($ch, CURLOPT_USERAGENT, '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        //curl_setopt($ch, CURLOPT_REFERER, 'http://domain.com/');
-        //if( !curl_setopt($ch, CURLOPT_HEADER, $curlopt_header)) return "FAIL: curl_setopt(CURLOPT_HEADER)";
-        if( !curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $redirects > 0)) return "FAIL: curl_setopt(CURLOPT_FOLLOWLOCATION)";
-        if( !curl_setopt($ch, CURLOPT_FILE, $fp) ) return "FAIL: curl_setopt(CURLOPT_FILE)";
-        if( !curl_setopt($ch, CURLOPT_MAXREDIRS, $redirects) ) return "FAIL: curl_setopt(CURLOPT_MAXREDIRS)";
-
-        return curl_exec($ch);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $redirects > 0);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, $redirects);
       } else {
         curl_setopt($ch, CURLOPT_USERAGENT, '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        //curl_setopt($ch, CURLOPT_REFERER, 'http://domain.com/');
-        if( !curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false)) return "FAIL: curl_setopt(CURLOPT_FOLLOWLOCATION)";
-        if( !curl_setopt($ch, CURLOPT_FILE, $fp) ) return "FAIL: curl_setopt(CURLOPT_FILE)";
-        if( !curl_setopt($ch, CURLOPT_HEADER, true)) return "FAIL: curl_setopt(CURLOPT_HEADER)";
-        if( !curl_setopt($ch, CURLOPT_RETURNTRANSFER, true)) return "FAIL: curl_setopt(CURLOPT_RETURNTRANSFER)";
-        if( !curl_setopt($ch, CURLOPT_FORBID_REUSE, false)) return "FAIL: curl_setopt(CURLOPT_FORBID_REUSE)";
-        curl_setopt($ch, CURLOPT_USERAGENT, '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071204 Ubuntu/7.10 (gutsy) Firefox/2.0.0.11');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, false);
       }
-      // if( !curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true) ) return "FAIL: curl_setopt(CURLOPT_FOLLOWLOCATION)";
-      // if( !curl_setopt($ch, CURLOPT_FILE, $fp) ) return "FAIL: curl_setopt(CURLOPT_FILE)";
-      // if( !curl_setopt($ch, CURLOPT_HEADER, 0) ) return "FAIL: curl_setopt(CURLOPT_HEADER)";
-      if(!curl_exec($ch))
-        return "FAIL: curl_exec()";
+      curl_exec($ch)
       curl_close($ch);
       fclose($fp);
-      return "SUCCESS: $file [$url]";
     }
-    return "FAIL: fopen()";
   }
-  return "FAIL: curl_init()";
 }
 
 /**
