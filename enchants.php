98aa67e331506b8a98a0c9c0165e0a39b036f3f3
David Franke <mirroar@gmail.com>
2014-10-21 21:41:39 +0200

    added parsing for most common stats

--- enchants.php
+++ enchants.php
@@ -8,9 +8,12 @@ define('ENCHANT_REGEX', '#_\\[(\\d+)\\]=(\\{[^\\}]*\\});#');
 define('ENCHANT_SPELL_REGEX', '#\\(\\$WH\\.g_enhanceTooltip\\.bind\\(tt\\)\\)\\(ITEM_ID,[^\\[\\)]*\\[(\\d+)\\][^\\)]*\\)#');
 define('ENCHANT_BASE_REGEX', '#_\\[SPELL_ID\\]=(\\{[^\\}]*\\});#');
 define('ENCHANT_TOOLTIP_REGEX', '#_\\[SPELL_ID\\]\\.tooltip_enus = \'(.*)\';#');
-define('ENCHANT_ID_REGEX', '#<td[^>]*>Enchant Item\\: [^\\(]*\\((\\d+)\\)</td>#');
+define('ENCHANT_ID_REGEX', '#<td[^>]*>Enchant Item\\: [^\\(]*\\((\\d+)\\)</td>#i');
 
-define('TOOLTIP_SIMPLE_EFFECT_REGEX', '#Permanently enchants? (.*?) [ts]o (.*)\.#');
+define('TOOLTIP_MAX_LEVEL_REGEX', '#Cannot be applied to items higher than level (\\d+)#i');
+define('TOOLTIP_SIMPLE_EFFECT_REGEX', '#Permanently enchants? (.*?) [ts]o (.*)\\.#i');
+define('TOOLTIP_TIMED_STAT_REGEX', '#(?:sometimes)? increase (.*?) by (\\d+) for (\\d+) sec#i');
+define('TOOLTIP_SIMPLE_STAT_REGEX', '#increase (.*?) by (\\d+)#i');
 
 // map targets from tooltip to item locations
 // TODO: use slot numbers from WoW
@@ -34,6 +37,59 @@ $slot_mapping = array(
   'a shield' => 0,
   'a shield or off-hand item' => 0,
   'a shield or held item' => 0,
+);
+
+$stat_mapping = array(
+  'Agility' => 'ITEM_MOD_AGILITY_SHORT',
+  'your Agility' => 'ITEM_MOD_AGILITY_SHORT',
+  'Strength' => 'ITEM_MOD_STRENGTH_SHORT',
+  'Intellect' => 'ITEM_MOD_INTELLECT_SHORT',
+  'your Intellect' => 'ITEM_MOD_INTELLECT_SHORT',
+  'Stamina' => 'ITEM_MOD_STAMINA_SHORT',
+  'Spirit' => 'ITEM_MOD_SPIRIT_SHORT',
+  'Spirit and Stamina' => array(
+    'ITEM_MOD_STAMINA_SHORT',
+    'ITEM_MOD_SPIRIT_SHORT',
+  ),
+  'Stamina and Spirit' => array(
+    'ITEM_MOD_STAMINA_SHORT',
+    'ITEM_MOD_SPIRIT_SHORT',
+  ),
+  'all stats' => array(
+    'ITEM_MOD_AGILITY_SHORT',
+    'ITEM_MOD_STRENGTH_SHORT',
+    'ITEM_MOD_INTELLECT_SHORT',
+    'ITEM_MOD_STAMINA_SHORT',
+    'ITEM_MOD_SPIRIT_SHORT',
+  ),
+  'crit' => 'ITEM_MOD_CRIT_RATING_SHORT',
+  'critical strike' => 'ITEM_MOD_CRIT_RATING_SHORT',
+  'your critical strike' => 'ITEM_MOD_CRIT_RATING_SHORT',
+  'haste' => 'ITEM_MOD_HASTE_RATING_SHORT',
+  'Haste' => 'ITEM_MOD_HASTE_RATING_SHORT',
+  'mastery' => 'ITEM_MOD_MASTERY_RATING_SHORT',
+  'multistrike' => 'ITEM_MOD_CR_MULTISTIKE_SHORT',
+  'versatility' => 'ITEM_MOD_VERSATILITY',
+  'parry' => 'ITEM_MOD_PARRY_RATING_SHORT',
+  'dodge' => 'ITEM_MOD_DODGE_RATING_SHORT',
+  'your dodge' => 'ITEM_MOD_DODGE_RATING_SHORT',
+  'Agility and dodge' => array(
+    'ITEM_MOD_AGILITY_SHORT',
+    'ITEM_MOD_DODGE_RATING_SHORT',
+  ),
+  'attack power' => 'ITEM_MOD_MELEE_ATTACK_POWER_SHORT',
+  'your attack power' => 'ITEM_MOD_MELEE_ATTACK_POWER_SHORT',
+  'spell power' => 'ITEM_MOD_SPELL_POWER_SHORT',
+  'PvP Power' => 'ITEM_MOD_PVP_POWER_SHORT',
+  'PvP power' => 'ITEM_MOD_PVP_POWER_SHORT',
+  'your PvP Power' => 'ITEM_MOD_PVP_POWER_SHORT',
+  'PvP Resilience' => 'ITEM_MOD_RESILIENCE_RATING_SHORT',
+  'armor' => 'RESISTANCE0_NAME',
+  'Bonus Armor' => 'RESISTANCE0_NAME',
+  'health' => array(), // usually irrelevant and not used by TopFit
+  'mana' => array(), // usually irrelevant and not used by TopFit
+  'movement speed' => array(), // currently not used by TopFit
+  'hit' => array(), // hit rating: gone!
 );
 
 // load www.wowhead.com/items=0.6 and go from there
@@ -83,17 +139,71 @@ foreach ($enchant_matches as $match) {
       $enchant['enchant_id'] = $enchant_id;
     }
 
-    // now begins the fun part: parsing the tooltip to get information about the enchant's effects
-    $parse_successful = FALSE;
+    if (!empty($enchant['enchant_id'])) {
+      // now begins the fun part: parsing the tooltip to get information about the enchant's effects
+      $parse_successful = FALSE;
 
-    $effect_matches = array();
-    preg_match_all(TOOLTIP_SIMPLE_EFFECT_REGEX, $enchant['tooltip'], $effect_matches, PREG_SET_ORDER);
-    foreach ($effect_matches as $effect) {
-      if (!isset($slot_mapping[$effect[1]])) {
-        debug('Unknown Effect Target: ' . $effect[1]);
+      $effect_matches = array();
+      $tt = $enchant['tooltip'];
+
+      // clean tooltip from unnecessary info
+      $tt = strip_tags($tt);
+
+      preg_match_all(TOOLTIP_MAX_LEVEL_REGEX, $tt, $effect_matches, PREG_SET_ORDER);
+      foreach ($effect_matches as $effect) {
+        $enchant['topfit']['requirements']['max_ilevel'] = $effect[1];
+        $tt = str_replace($effect[0], '', $tt);
       }
-      $parse_successful = TRUE;
-      //debug('Effect: ' . $effect[2]);
+
+      preg_match_all(TOOLTIP_SIMPLE_EFFECT_REGEX, $tt, $effect_matches, PREG_SET_ORDER);
+      foreach ($effect_matches as $effect) {
+        if (!isset($slot_mapping[$effect[1]])) {
+          debug('Unknown Effect Target: ' . $effect[1]);
+        }
+        //$parse_successful = TRUE;
+        //debug('Effect: ' . $effect[2]);
+        $stat_matches = array();
+        preg_match_all(TOOLTIP_TIMED_STAT_REGEX, $effect[2], $stat_matches, PREG_SET_ORDER);
+        foreach ($stat_matches as $stat) {
+          if (isset($stat_mapping[$stat[1]])) {
+            $stats = $stat_mapping[$stat[1]];
+            if (!is_array($stats)) {
+              $stats = array($stats);
+            }
+            foreach ($stats as $single_stat) {
+              $enchant['topfit']['stats'][$single_stat] = $stat[2] / $stat[3];
+            }
+            $parse_successful = TRUE;
+
+            $effect[2] = str_replace($stat[0], '', $effect[2]);
+          } else {
+            debug('Unknown stat (timed): ' . $stat[1]);
+          }
+        }
+
+        $stat_matches = array();
+        preg_match_all(TOOLTIP_SIMPLE_STAT_REGEX, $effect[2], $stat_matches, PREG_SET_ORDER);
+        foreach ($stat_matches as $stat) {
+          if (isset($stat_mapping[$stat[1]])) {
+            $stats = $stat_mapping[$stat[1]];
+            if (!is_array($stats)) {
+              $stats = array($stats);
+            }
+            foreach ($stats as $single_stat) {
+              $enchant['topfit']['stats'][$single_stat] = $stat[2];
+            }
+            $parse_successful = TRUE;
+
+            $effect[2] = str_replace($stat[0], '', $effect[2]);
+          } else {
+            debug('Unknown stat: ' . $stat[1]);
+          }
+        }
+      }
+
+      if (!$parse_successful) {
+        //debug('Could not parse enchant tooltip: ' . $tt);
+      }
     }
 
     break;
